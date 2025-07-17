c019138badeeb7c7f21818190c5d07698a9db185
Gurvinder Singh Bindra <gurribindra@gmail.com>
2025-07-17 16:16:55 +0000

    modifying startup script

--- setup_vjb.sh
+++ setup_vjb.sh
@@ -12,4 +12,12 @@ sudo apt-get -y install jq
 
 echo "*******Setting up VMware-vix-disklib*****" 
 cp VMware-vix-disklib* ~/.
+DIRECTORY="vmware-vix-disklib-distrib"
+if [ -d "$DIRECTORY" ]; then
+  echo "$DIRECTORY has been created."
+  tar -xvzf ~/VMware-vix-disklib*  ~/vmware-vix-disklib-distrib/
+fi
+
+
+
 tar -xvzf ~/VMware-vix-disklib*  ~/vmware-vix-disklib-distrib/
